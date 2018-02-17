13d2b94098fbad5a921feaeee4a8cdcb28dab7d5
JustMeDaFaq <paypalmax1991@yahoo.de>
2018-02-17 14:50:27 +0100

    init

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-module.exports = test;
+module.exports = openload;
 
-function test () {
-  return "test"
+function openload (link) {
+  return new Promise(
+    function (resolve, reject) {
+
+                (async () => {
+                  const browser = await puppeteer.launch();
+                  const page = await browser.newPage();
+                  await page.goto(link);
+                  let bodyHTML = await page.evaluate(() => document.getElementById("streamurj").textContent);//this.document.body.innerHTML);//this.document.body.innerHTM;
+                  resolve(bodyHTML); 
+                  await browser.close();
+                  
+                })()
+
+
+              // fulfilled
+       } /*else {
+                var reason = new Error('mom is not happy');
+                reject(reason); // reject
+            }
+            */
+        
+    );
 };
